2b2cae7556a8f64a659cc1d067e3556e279833bd
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-06-02 16:54:36 +0300

    feat(sveltekit): create agent data state store with data fetcher function
    First create `src/lib/global.d.ts` file and import the `Agent` type
    into it from the core package so the type becomes declared in
    the whole `sveltekit` package.
    
    Create `/src/lib/store/agent.svelte.ts` file and create inside it the
    agent data state store and `fetchAgentData` function which is
    responsible for fetching agent's data.

---
+++ packages/sveltekit/src/lib/store/agent.svelte.ts
@@ -0,0 +1,29 @@
+type AgentState = {
+    agent: Agent | null;
+    loading: boolean;
+    error: string;
+}
+
+export const agentState = $state<AgentState>({
+    agent: null,
+    loading: false,
+    error: ""
+});
+
+export function fetchAgentData() {
+    if (!agentState.loading) {
+        agentState.loading = true
+        fetch("/api/mecha-agent?target=agent-data")
+            .then((res) => res.json())
+            .then((res) => {
+                if (res.result) {
+                    agentState.agent = res.result as Agent;
+                    agentState.error = "";
+                } else {
+                    agentState.error = res.error;
+                }
+            })
+            .catch((error) => { agentState.error = error.message })
+            .finally(() => { agentState.loading = false })
+    }
+}
