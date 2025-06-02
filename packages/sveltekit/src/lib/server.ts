7a6644eebc82073708461a8780ffe942b7681c57
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-06-02 13:48:31 +0300

    feat(sveltekit): implement the api routes handlers for `Sveltekit`
    Create `src/lib/server.ts` file and inside it implement the
    api routes handlers of the `Sveltekit` package using the server
    logic utilities from the core package.

---
+++ packages/sveltekit/src/lib/server.ts
@@ -0,0 +1,105 @@
+import type { RequestEvent } from "@sveltejs/kit";
+import { GetAgentData, SendPrompt, GetChatMessages, GetAgentAvatar } from "@mecha-agent-inference-client/core/server";
+
+type MechaAgentRouteHandlerConfig = {
+    agentId: string;
+    apiKey: string;
+}
+
+function GetAgentDataHandler(config: MechaAgentRouteHandlerConfig) {
+    return async function () {
+        const { body, status } = await GetAgentData({
+            agentId: config.agentId,
+            apiKey: config.apiKey,
+        })
+
+        return new Response(
+            body instanceof ReadableStream ? body : JSON.stringify(body),
+            { status }
+        );
+    }
+}
+
+function GetAgentAvatarHandler(config: MechaAgentRouteHandlerConfig) {
+    return async function (e: RequestEvent) {
+        const { body, status } = await GetAgentAvatar({
+            agentId: config.agentId,
+            apiKey: config.apiKey,
+            avatarId: e.url.searchParams.get("avatarId")?.toString()
+        })
+
+        return new Response(
+            body instanceof ReadableStream ? body : JSON.stringify(body),
+            { status }
+        );
+    }
+}
+
+function GetChatMessagesHandler(config: MechaAgentRouteHandlerConfig) {
+    return async function (e: RequestEvent) {
+        const { body, status } = await GetChatMessages({
+            agentId: config.agentId,
+            apiKey: config.apiKey,
+            chatId: e.url.searchParams.get("chatId")?.toString(),
+        })
+
+        return new Response(
+            body instanceof ReadableStream ? body : JSON.stringify(body),
+            { status }
+        );
+    }
+};
+
+function SendPromptHandler(config: MechaAgentRouteHandlerConfig) {
+    return async function (e: RequestEvent) {
+        const json = await e.request.json()
+        const { body, status, headers } = await SendPrompt({
+            agentId: config.agentId,
+            apiKey: config.apiKey,
+            chatId: e.url.searchParams.get("chatId")?.toString(),
+            prompt: json?.prompt
+        })
+
+        return new Response(
+            body instanceof ReadableStream ? body : JSON.stringify(body),
+            { status, headers: { chatId: (headers?.chatId || "") as string } }
+        );
+    }
+};
+
+export function handler(config: MechaAgentRouteHandlerConfig) {
+    return function (e: RequestEvent) {
+        switch (e.request.method) {
+            case 'GET': {
+                switch (e.url.searchParams.get("target")) {
+                    case "agent-data": {
+                        return GetAgentDataHandler(config)()
+                    }
+
+                    case "agent-avatar": {
+                        return GetAgentAvatarHandler(config)(e)
+                    }
+
+                    case "chat-messages": {
+                        return GetChatMessagesHandler(config)(e)
+                    }
+
+                    default: {
+                        return new Response("Unknown target", { status: 400 });
+                    }
+                }
+            }
+            case 'POST': {
+                if (e.url.searchParams.get("target") === "chat") {
+                    return SendPromptHandler(config)(e)
+                }
+
+                return new Response("Unknown target", { status: 400 });
+            }
+
+            default: {
+                return new Response("Method Not Allowed", { status: 405 });
+            }
+        }
+    }
+};
