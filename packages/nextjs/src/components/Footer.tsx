7756ffd81f04d6555ae6c1a174a13b91ba9d163c
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-06-03 14:20:48 +0300

    feat(nextjs, sveltekit): create `Footer` component

---
+++ packages/nextjs/src/components/Footer.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import logo from "@mecha-agent-inference-client/core/logo.webp";
+import { footerLinks } from "@mecha-agent-inference-client/core/client";
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            <a
+                href={footerLinks.mechaAgentAppUrl}
+                target="_blank"
+                className="footer-logo"
+            >
+                <Image
+                    width={25}
+                    height={25}
+                    className="footer-image"
+                    src={logo}
+                    alt="Mecha Agent Logo"
+                />
+                Mecha Agent
+            </a>
+            <p className="footer-credit">
+                <span>Made by</span>
+                <a href={footerLinks.madeBy.link} target="_blank" className="footer-link">
+                    {footerLinks.madeBy.text}
+                </a>
+            </p>
+            <div className="footer-links">
+                <a href={footerLinks.docs.link} target="_blank" className="footer-link">
+                    {footerLinks.docs.text}
+                </a>
+                -
+                <a
+                    href={footerLinks.termsAndConditions.link}
+                    target="_blank"
+                    className="footer-link"
+                >
+                    {footerLinks.termsAndConditions.text}
+                </a>
+            </div>
+        </div>
+    )
+};
---
+++ packages/sveltekit/src/lib/components/Footer.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    import logo from "@mecha-agent-inference-client/core/logo.webp";
+    import { footerLinks } from "@mecha-agent-inference-client/core/client";
+</script>
+
+<div class="footer">
+    <a href={footerLinks.mechaAgentAppUrl} target="_blank" class="footer-logo">
+        <img
+            width={25}
+            height={25}
+            class="footer-image"
+            src={logo}
+            alt="Mecha Agent Logo"
+        />
+        Mecha Agent
+    </a>
+    <p class="footer-credit">
+        <span>Made by</span>
+        <a href={footerLinks.madeBy.link} target="_blank" class="footer-link">
+            {footerLinks.madeBy.text}
+        </a>
+    </p>
+    <div class="footer-links">
+        <a href={footerLinks.docs.link} target="_blank" class="footer-link">
+            {footerLinks.docs.text}
+        </a>
+        -
+        <a
+            href={footerLinks.termsAndConditions.link}
+            target="_blank"
+            class="footer-link"
+        >
+            {footerLinks.termsAndConditions.text}
+        </a>
+    </div>
+</div>
