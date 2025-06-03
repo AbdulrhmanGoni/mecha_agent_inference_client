d01e09a232048538043ee3625f12adc657d66ae8
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-06-03 14:21:21 +0300

    feat(sveltekit): create three loading state components
    - Create `LoadingAgent` component to show a loading state UI
    while fetching agent data.
    
    - Create `LoadingChatMessages` component to show a loading
    state UI while fetching the messages of the most recent chat
    session.
    
    - Create `IsTypingState` component to show a typing state
    while generating an agent (or LLM) response.

---
+++ packages/sveltekit/src/lib/components/IsTypingState.svelte
@@ -0,0 +1,5 @@
+<div class="is-typing-container">
+    <div class="is-typing-dot dot-1"></div>
+    <div class="is-typing-dot dot-2"></div>
+    <div class="is-typing-dot dot-3"></div>
+</div>
---
+++ packages/sveltekit/src/lib/components/LoadingAgent.svelte
@@ -0,0 +1,8 @@
+<div class="loading-agent-container">
+    <div class="loading-agent-bar pulse"></div>
+    <div class="loading-agent-body">
+        <div class="loading-agent-greeting-message pulse"></div>
+        <div class="loading-agent-prompt-input pulse"></div>
+    </div>
+    <div class="loading-agent-bar pulse"></div>
+</div>
