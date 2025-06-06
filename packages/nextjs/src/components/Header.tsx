96edd8932de513c22fc006803ed72597e4e03f1e
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-06-07 01:28:38 +0300

    feat(nextjs, sveltekit): create `Header` component

---
+++ packages/nextjs/src/components/Header.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import { useContext } from "react";
+import { chatContext } from "./ChatContextProvider";
+import SwitchTheme from "./SwitchTheme";
+import { createAgentAvatarUrl } from "@mecha-agent-inference-client/core/client";
+
+export default function Header({ agent }: { agent: Agent }) {
+    const state = useContext(chatContext)
+
+    function startNewChat() {
+        state.resetState()
+    }
+
+    return (
+        <div className="header">
+            <div className="agent-info">
+                {
+                    agent.avatar ?
+                        <Image
+                            width={48}
+                            height={48}
+                            className="avatar"
+                            src={createAgentAvatarUrl(agent.avatar)}
+                            alt="Agent Avatar"
+                        /> : <span className="avatar agent-fallback-avatar" />
+                }
+                <div>
+                    <p className="agent-name">{agent.agentName}</p>
+                    <p className="agent-description">{agent.description}</p>
+                </div>
+            </div>
+            <div className="actions">
+                <SwitchTheme />
+                <button
+                    className="new-chat-button"
+                    onClick={startNewChat}
+                >
+                    New Chat
+                </button>
+            </div>
+        </div>
+    )
+};
